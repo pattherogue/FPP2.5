f17b0209b6655ac408c596305c839d3ee0e0783b
Paolo Gomez <peweegardner248@gmail.com>
2024-01-15 06:11:41 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const guests = ["ANTONY", "CICERO", "CASSIUS", "CLEOPATRA"];
+
+//Caesar remembers he forgot to add his best friend "BRUTUS" to the list. Add him to the beginning of the list.
+
+guests.unshift("BRUTUS");
